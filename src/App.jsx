dd80a6fe8a9ee54e111f2056d183dfba4279159d
Zain Ali <ZainSoftwear11@gmail.com>
2023-04-19 11:48:48 +0500

    Gym-Exercise App Created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+import ExerciseDetail from "./Pages/ExerciseDetail";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
 
-const App = () => {
-    return (
-        <div>App</div>
-    );
-};
+const App = () => (
+    <>
+        <Box
+            width="400px">
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/exercise/:id' element={<ExerciseDetail />} />
+            </Routes>
+        </Box>
+    </>
+);
 
 export default App;
